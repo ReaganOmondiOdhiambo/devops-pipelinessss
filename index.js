fe1150afe3d708798209884696d29d4ef4b05986
rEAGANoMONDOoDHIAMB <omondireagan2001@gmail.com>
2025-11-18 00:17:25 +0300

    feature add pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello devops")
+console.log("Learning cicd using github-actins")
---
+++ text.js
@@ -0,0 +1,3 @@
+console.log("Starting all the tests..................")
+setTimeout(()=> console.log("All tests passed successfully!"), 3000);
+console.log("Deploying to production server...")
